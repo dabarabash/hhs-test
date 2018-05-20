a0dcc2473b1a209dad543cadbbebe1e234b76a44
dbarabash <c3-po@ukr.net>
2018-05-21 00:31:21 +0300

    corrections

--- task2.sh
+++ task2.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
+## obtaining code
 git -C ~ clone https://github.com/hhstechgroup/sample-application
 cd ~/sample-application
+## executing build application
 ./gradlew clean bootJar
+## run tests
 ./gradlew test
-cd ~/sample-application/build/libs
-echo -e "FROM openjdk:jre\nMAINTAINER dmitriy_barabash\nWORKDIR ~/sample-application/build/libs\nCOPY sample-application-0.0.1-SNAPSHOT.jar ~/$
+## making Dockerfile
+echo -e "FROM openjdk:jre\nMAINTAINER dmitriy_barabash\nCOPY ./build/libs/sample-application-0.0.1-SNAPSHOT.jar sample-application-0.0.1-SNAPSHOT.jar\nEXPOSE 8883\nCMD [\"java\",\"-jar\",\"sample-application-0.0.1-SNAPSHOT.jar\"]" > Dockerfile
+## build Docker image
 docker build -t samp_app_img .
+## running build image
 docker run -d -p 8883:8883 samp_app_img
+## waiting for website uptime
 while [ -n TRUE ]; do
     STATUS_CODE=`curl -Is http://localhost:8883/sample/tests | head -n1 | awk '{print($2)}'`
     if [ -n "$STATUS_CODE" ]; then
-         echo "Site is up :)"
+        echo "Site is up :)"
 break
     fi
 
     if [ -z "$STATUS" ]; then
-         echo "Site is down :("
+        echo "Site is down :("
     fi
     sleep 3
 done
+## stopping image
 stop=`docker ps | grep 'samp_app_img' | awk ' { print $1 } ' | xargs docker stop`
-echo 'Enter your docker-account login'
-read login
-docker login -u "$login"
-docker tag samp_app_img "$login"/samp_app_img
-docker push "$login"/samp_app_img
-echo "Check your Docker repo"
+## naming Docker image
+docker tag samp_app_img dbarabash/samp_app_img
+## Pushing image to docker hub
+docker push dbarabash/samp_app_img
+echo "Now you may check your pushed image on Docker hub"
+rm -rf ~/sample-application
+
